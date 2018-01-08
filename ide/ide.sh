cb1c9121b2aabcc99417c0b1654616f7183d51e8
pavlo <pavlo@tubularlabs.com>
2018-01-08 03:30:21 +0200

    kind of ide

---
+++ ide/ide.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+
+docker build -t tbcode/spacemacs --build-arg SUBDIR=$1 .
+docker run -it -e DISPLAY=$ip:0 -e SUBDIR=$1 tbcode/spacemacs bash
