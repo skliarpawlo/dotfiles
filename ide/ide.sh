e467b17dc74b1ad33e0acf9168eb6ee471dca7fc
pavlo <pavlo@tubularlabs.com>
2018-01-08 18:45:54 +0200

    refactor dockers

--- ide/ide.sh
+++ ide/ide.sh
@@ -2,5 +2,5 @@
 
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 
-docker build -t tbcode/spacemacs --build-arg SUBDIR=$1 .
-docker run -it -e DISPLAY=$ip:0 -e SUBDIR=$1 tbcode/spacemacs bash
+docker build -t tbcode/$1 --build-arg SUBDIR=$1 .
+docker run -it -e DISPLAY=$ip:0 -e SUBDIR=$1 tbcode/$1 bash
