7622f77b41c2d83eb9480d5e96e8defd340b9ca4
pavlo <pavlo@tubularlabs.com>
2018-02-02 15:58:21 +0200

    FIX ide open on branch

--- ide/ide.sh
+++ ide/ide.sh
@@ -2,5 +2,5 @@
 
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 
-docker build -t tbcode/$1 --build-arg SUBDIR=$1 .
-docker run -it -e DISPLAY=$ip:0 -e SUBDIR=$1 tbcode/$1 bash
+docker build -t tbcode/$1 --build-arg SUBDIR=$1 --build-arg BRANCH=$2 .
+docker run -it -e DISPLAY=$ip:0 -e SUBDIR=$1 -e BRANCH=$2 tbcode/$1 bash
--- ide/init.sh
+++ ide/init.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-cd /code && git pull
+cd /code && git pull && git checkout $BRANCH
+pip3.5 install -r /code/$SUBDIR/requirements.txt
+pip3.5 install -r /code/$SUBDIR/requirements_dev.txt
 
 emacs -fs &
 
